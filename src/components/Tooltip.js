3b422e6de2c7321381c77b57f11a0e6fef9eecf0
Shweta Rajendra Saindane <80962110+shweta27-alt@users.noreply.github.com>
2023-09-03 19:38:01 +0530

    tooltip

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -3,12 +3,36 @@ import './Tooltip.css'
 
 const Tooltip = () => {
   return (
+    <>
     <div className="container">
        {/* <h4 >tooltip on top</h4> */}
        <div className="tooltip top" title="tooltip on top">
         Hover over me
        </div>
+
     </div>
+
+     <div className="container">
+       {/* <h4 >tooltip on top</h4> */}
+       <div className="tooltip bottom" title="tooltip on bottom">
+        Hover over me
+       </div>
+    </div>
+
+    <div className="container">
+       {/* <h4 >tooltip on top</h4> */}
+       <div className="tooltip left" title="tooltip on left">
+        Hover over me
+       </div>
+    </div>
+
+    <div className="container">
+       {/* <h4 >tooltip on top</h4> */}
+       <div className="tooltip right" title="tooltip on right">
+        Hover over me
+       </div>
+    </div>
+    </>
   )
 }
 
